fd210453dc3ce9851727b1540b82fa47df3dad7b
Axel Marazzi <marazzi.axel@gmail.com>
2018-05-11 20:45:06 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,13 @@
 # script autoinstall
 #
 
-echo Installing Homebrew
+echo "Installing Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "Setting up apps"
 
 brew cask install zsh tmux hyper wget google-chrome git sublime-text3 caffeine spotify aria2c vlc 7zX chromecast cyberduck irssi bwn-ng htop
+
+# download wallpaper
+
+wget --no-check-certificate -P $HOME/Pictures "https://i.imgur.com/2nc9JQo.jpg"
